9a0ca4dfffc6641e28a829dc8b054a32524a0eeb
Andrey Kuzmin <prepodam@mail.ru>
2018-03-25 04:06:21 +0300

    - Remove buggy RELP support. Rsyslog version not compatible with Go. - Add syslog protocol support.

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Accept and write logs to Clickhouse in Loghouse compatible format.
 
-Daemon accepts log only in RELP format from rsyslog. Later may be will be added
+Daemon accepts log only in syslog RFC5424 format from rsyslog. Later may be will be added
 some additinal protocols support.
 
 ## Configuration
@@ -21,6 +21,12 @@ Batch size to send to Clickhouse immediately
 
 ACC_BATCH=10000
 
+Configure rsyslog to resend logs to daemon
+
+```
+action(type="omfwd" Target="127.0.0.1" Port="3333" Protocol="tcp" KeepAlive="on" Template="RSYSLOG_SyslogProtocol23Format")
+```
+
 ## Running
 
-docker run --rm -it -p 3333:3333 -e CLICKHOUSE_ADDR=cloud-11.dd:9000 kaktuss/loghouse-acceptor
+docker run --rm -it -p 3333:3333 -e CLICKHOUSE_ADDR=127.0.0.1:9000 kaktuss/loghouse-acceptor
--- main.go
+++ main.go
@@ -2,31 +2,25 @@ package main
 
 import (
 	"database/sql"
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
 	"strconv"
+	"strings"
 	"syscall"
 	"time"
 
 	"github.com/kshvakov/clickhouse"
-	relp "github.com/stith/gorelp"
+	syslog "gopkg.in/mcuadros/go-syslog.v2"
 )
 
 type reqType struct {
 	Time          string `json:"time"`
 	Tag           string `json:"tag"`
-	Host          string `json:"host"`
-	IP            string `json:"ip"`
-	Source        string `json:"source"`
 	Hostname      string `json:"hostname"`
 	Program       string `json:"program"`
-	Priority      uint16 `json:"priority"`
-	Severity      uint16 `json:"severity"`
-	Facility      uint16 `json:"facility"`
 	SeverityLabel string `json:"severity_label"`
 	FacilityLabel string `json:"facility_label"`
 	Msg           string `json:"msg"`
@@ -46,7 +40,7 @@ func main() {
 	stopChan := make(chan int)
 	go aggregate(ch, stopChan)
 
-	go listen(ch)
+	go listenSyslog(ch)
 	go healthcheck()
 
 	<-stopChan
@@ -75,37 +69,59 @@ func connectDB() {
 	return
 }
 
-func listen(ch chan reqType) {
-	relpServer, err := relp.NewServer("0.0.0.0", 3333, true)
+func listenSyslog(ch chan reqType) {
+	channel := make(syslog.LogPartsChannel)
+	handler := syslog.NewChannelHandler(channel)
+
+	server := syslog.NewServer()
+	server.SetFormat(syslog.RFC5424)
+	server.SetHandler(handler)
+
+	err := server.ListenTCP("0.0.0.0:3333")
 	if err != nil {
 		errLogger.Panicln(err)
 	}
 
+	server.Boot()
+
 	stopSignal := make(chan os.Signal, 1)
 	signal.Notify(stopSignal, syscall.SIGINT, syscall.SIGTERM)
 
-	for {
-		select {
-		case message := <-relpServer.MessageChannel:
-			var parsed reqType
+	severityLabels := map[int]string{0: "emerg", 1: "alert", 2: "crit", 3: "error",
+		4: "warning", 5: "notice", 6: "info", 7: "debug"}
+	facilityLabels := map[int]string{0: "kern", 1: "user", 2: "mail", 3: "daemon",
+		4: "auth", 5: "syslog", 6: "lpr", 7: "news", 8: "uucp", 9: "cron", 10: "security",
+		11: "ftp", 12: "ntp", 13: "logaudit", 14: "logalert", 15: "clock", 16: "local0",
+		17: "local1", 18: "local2", 19: "local3", 20: "local4", 21: "local5",
+		22: "local6", 23: "local7"}
 
-			err = json.Unmarshal([]byte(message.Data), &parsed)
-			if err != nil {
-				errLogger.Println(err, ":", message.Data)
-				continue
+	go func(channel syslog.LogPartsChannel) {
+		for {
+			select {
+			case message := <-channel:
+				parsed := reqType{
+					Time:          message["timestamp"].(time.Time).Format(time.RFC3339),
+					Tag:           message["proc_id"].(string),
+					Hostname:      message["hostname"].(string),
+					Program:       message["app_name"].(string),
+					SeverityLabel: severityLabels[message["severity"].(int)],
+					FacilityLabel: facilityLabels[message["facility"].(int)],
+					Msg:           strings.TrimLeft(message["message"].(string), " ")}
+
+				ch <- parsed
+			case <-stopSignal:
+				logger.Println("Got stop signal, stop listening Syslog")
+				server.Kill()
+
+				logger.Println("Close channel")
+				close(ch)
+
+				return
 			}
-
-			ch <- parsed
-		case <-stopSignal:
-			logger.Println("Got stop signal, stop listening RELP")
-			relpServer.Close()
-
-			logger.Println("Close channel")
-			close(ch)
-
-			return
 		}
-	}
+	}(channel)
+
+	server.Wait()
 }
 
 func aggregate(ch chan reqType, stopChan chan int) {
@@ -162,7 +178,7 @@ func send(vals []reqType) {
 	for i := 0; i < len(vals); i++ {
 		dt, err := time.Parse(time.RFC3339, vals[i].Time)
 		if err != nil {
-			errLogger.Println("RELP date parse failed: ", err)
+			errLogger.Println("RFC3339 date parse failed: ", err)
 			continue
 		}
 
@@ -208,7 +224,7 @@ func send(vals []reqType) {
 			boolValues := [0]int{}
 			nullNames := [0]string{}
 
-			_, err := stmt.Exec(val.ToCHdate, val.ToCHtimestamp, val.ToCHnsec, val.Source,
+			_, err := stmt.Exec(val.ToCHdate, val.ToCHtimestamp, val.ToCHnsec, "",
 				val.FacilityLabel, val.Hostname, val.Tag, val.Program, val.SeverityLabel,
 				labelsNames, labelsValues, stringNames, stringValues, numbersNames,
 				numbersValues, boolNames, boolValues, nullNames, 79031234567,
