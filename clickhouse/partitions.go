e96e9f4593693cd1a9e3be0fff3d60806f58f27e
Andrey Kuzmin <prepodam@mail.ru>
2018-05-01 20:59:17 +0300

    Sharded and replicated tables.
    We now have shards and replicas.
    + some create refactoring.

--- clickhouse/partitions.go
+++ clickhouse/partitions.go
@@ -31,43 +31,78 @@ func (d *DB) CreatePartitions(partitionFormat string) {
 		dt := time.Now().Add(-time.Hour * 24)
 
 		for i := 1; i <= 48; i++ {
-			partition := dt.Format(partitionFormat)
+			d.create(dt.Format(partitionFormat))
 			dt = dt.Add(time.Hour)
-
-			sql := "SELECT 1 FROM system.tables WHERE database = 'logs'	AND name = 'logs" +
-				partition + "';"
-
-			rows, err := d.DB.Query(sql)
-			if err != nil {
-				d.errLogger.Println(err)
-				continue
-			}
-
-			if rows.Next() {
-				rows.Close()
-				continue
-			}
-
-			rows.Close()
-			d.logger.Println("Create partition " + partition)
-
-			sql = "CREATE TABLE logs.logs" + partition +
-				" ( date Date, timestamp DateTime, nsec UInt32, namespace String," +
-				" level String, tag String, host String, pid String, caller String," +
-				" msg String, labels Nested ( names String, values String )," +
-				" string_fields Nested ( names String, values String )," +
-				" number_fields Nested ( names String, values Float64 )," +
-				" boolean_fields Nested ( names String, values UInt8 )," +
-				" `null_fields.names` Array(String), phone UInt64, request_id String," +
-				" order_id String, subscription_id String )" +
-				" ENGINE = MergeTree( date, ( timestamp, nsec, level, tag, host," +
-				" phone, request_id, order_id, subscription_id ), 32768 );"
-
-			_, err = d.DB.Exec(sql)
-			if err != nil {
-				d.errLogger.Println(err)
-				continue
-			}
 		}
 	}
+}
+
+func (d *DB) create(partition string) {
+	exists, err := d.exists("logs" + partition)
+	if err != nil || exists {
+		return
+	}
+
+	d.logger.Println("Create partition logs" + partition)
+
+	sql := "CREATE TABLE IF NOT EXISTS logs.logs" + partition +
+		" ON CLUSTER so" +
+		" ( date Date, timestamp DateTime, nsec UInt32, namespace String," +
+		" level String, tag String, host String, pid String, caller String," +
+		" msg String, labels Nested ( names String, values String )," +
+		" string_fields Nested ( names String, values String )," +
+		" number_fields Nested ( names String, values Float64 )," +
+		" boolean_fields Nested ( names String, values UInt8 )," +
+		" `null_fields.names` Array(String), phone UInt64, request_id String," +
+		" order_id String, subscription_id String )" +
+		" ENGINE = Distributed( 'so', 'logs', 'logs" + partition +
+		"_shard', rand() );"
+
+	_, err = d.DB.Exec(sql)
+	if err != nil {
+		d.errLogger.Println(err)
+		return
+	}
+
+	sql = "CREATE TABLE IF NOT EXISTS logs.logs" + partition +
+		"_shard ON CLUSTER so" +
+		" ( date Date, timestamp DateTime, nsec UInt32, namespace String," +
+		" level String, tag String, host String, pid String, caller String," +
+		" msg String, labels Nested ( names String, values String )," +
+		" string_fields Nested ( names String, values String )," +
+		" number_fields Nested ( names String, values Float64 )," +
+		" boolean_fields Nested ( names String, values UInt8 )," +
+		" `null_fields.names` Array(String), phone UInt64, request_id String," +
+		" order_id String, subscription_id String )" +
+		" ENGINE = ReplicatedMergeTree( " +
+		"'/clickhouse/tables/{shard}/logs_logs" + partition +
+		"_shard', '{replica}'," +
+		" date, ( timestamp, nsec, level, tag, host," +
+		" phone, request_id, order_id, subscription_id ), 32768 );"
+
+	_, err = d.DB.Exec(sql)
+	if err != nil {
+		d.errLogger.Println(err)
+		return
+	}
+}
+
+func (d *DB) exists(table string) (bool, error) {
+	sql := "SELECT 1 FROM system.tables " +
+		"WHERE database = 'logs'	AND name = '" +
+		table + "';"
+
+	rows, err := d.DB.Query(sql)
+	if err != nil {
+		d.errLogger.Println(err)
+		return false, err
+	}
+
+	if rows.Next() {
+		rows.Close()
+		return true, nil
+	}
+
+	rows.Close()
+	return false, nil
 }
